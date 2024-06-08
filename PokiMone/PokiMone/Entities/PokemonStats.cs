d9fb44b40b130df7b81357233adbd69da782afdf
Rui Dias <ruippdias99@gmail.com>
2024-06-08 13:40:25 +0100

    Create base entities

---
+++ PokiMone/PokiMone/Entities/PokemonStats.cs
@@ -0,0 +1,13 @@
+﻿namespace PokiMone.Entities
+{
+    public class PokemonStats
+    {
+        public int Hp {  get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpAttack { get; set; }
+        public int SpDefense { get; set; }
+        public int Speed { get; set; }
+        public int Total { get; set; }
+    }
+}
